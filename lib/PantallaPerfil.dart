86b2a43de359d247c20d90289c2e5bae687d3401
MarioGangosoRibera <mario.ganfer@educa.jcyl.es>
2025-01-02 18:01:45 +0100

    hecha la pantalla perfil menos la funcion del boton

--- lib/PantallaPerfil.dart
+++ lib/PantallaPerfil.dart
@@ -1,10 +1,74 @@
 import 'package:flutter/material.dart';
+import 'Colores.dart';
 
 class PantallaPerfil extends StatelessWidget {
 
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/imagenes/LogoAlba.png', height: 200),
+            Text(
+              'E-mail',
+              style: TextStyle(
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            Text(
+              'info@albadonadonutricion.com',
+              style: TextStyle(
+                fontWeight: FontWeight.bold
+              ),  
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Telefono',
+              style: TextStyle(
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            Text(
+              '+34 698911465',
+              style: TextStyle(
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Consulta presencial',
+              style: TextStyle(
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            Text(
+              'LoMás Recoletas Salud (Valladolid) C/Juan Antonio Morales Pintor',
+              style: TextStyle(
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: 200,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: colorBoton
+                ),
+                onPressed: (){
+
+                }, 
+                child: Text(
+                  'Cerrar sesion',
+                  style: TextStyle(color: colorLetraB),  
+                )
+              )
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
