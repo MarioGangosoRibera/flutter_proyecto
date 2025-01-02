fd0a027e5eee37e1386da77c92dc4c6fee7ccb5c
MarioGangosoRibera <mario.ganfer@educa.jcyl.es>
2025-01-02 21:02:08 +0100

    Validar que los campos esten rellenos

--- lib/PantallaLogin.dart
+++ lib/PantallaLogin.dart
@@ -5,70 +5,92 @@ import 'PantallaServicios.dart';
 
 class PantallaLogin extends StatelessWidget {
 
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/imagenes/LogoAlba.png', height: 200),
-            Container(
-              width: 300,
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Correo electrónico',
-                  filled: true
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/imagenes/LogoAlba.png', height: 200),
+              Container(
+                width: 300,
+                child: TextFormField(
+                  validator: (value){
+                    if(value!.isEmpty){
+                      return 'El correo electrónico debe estar relleno';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Correo electrónico',
+                    filled: true
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 10,),
-            Container(
-              width: 300,
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Contraseña',
-                  filled: true
+              SizedBox(height: 10,),
+              Container(
+                width: 300,
+                child: TextFormField(
+                  validator: (value){
+                    if(value!.isEmpty){
+                      return 'La contraseña debe estar relleno';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Contraseña',
+                    filled: true
+                  ),
+                  obscureText: true,
                 ),
-                obscureText: true,
               ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              width: 200,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: colorBoton
-                ),
-                onPressed: (){
-                  Navigator.push(
-                    context, 
-                    MaterialPageRoute(builder: (context) => PantallaServicios()),
-                  );
-                }, 
-                child: Text(
-                  'Iniciar sesión',
-                  style: TextStyle(color: colorLetraB)
-                )
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('¿No tienes cuenta?'),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                    context, 
-                    MaterialPageRoute(builder: (context) => PantallaRegistro()),
-                    );      
+              SizedBox(height: 10),
+              Container(
+                width: 200,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: colorBoton
+                  ),
+                  onPressed: (){
+                    if(formKey.currentState!.validate()){
+                      //Debe ir la logica del iniciar sesion
+                      //De momento lo pongo asi para poder entrar a las siguientes pantallas
+                      Navigator.push(
+                      context, 
+                      MaterialPageRoute(builder: (context) => PantallaServicios()),
+                    );
+                    }
+                    
                   }, 
-                  child: Text('Registrarse')
-                )
-              ],
-            )
-          ],
+                  child: Text(
+                    'Iniciar sesión',
+                    style: TextStyle(color: colorLetraB)
+                  )
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('¿No tienes cuenta?'),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                      context, 
+                      MaterialPageRoute(builder: (context) => PantallaRegistro()),
+                      );      
+                    }, 
+                    child: Text('Registrarse')
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/PantallaResgistro.dart
+++ lib/PantallaResgistro.dart
@@ -5,87 +5,119 @@ import 'Colores.dart';
 
 class PantallaRegistro extends StatelessWidget {
 
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Registro')),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/imagenes/LogoAlba.png', height: 200),
-            Container(
-              width: 300,
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Nombre y apellidos',
-                  filled: true
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/imagenes/LogoAlba.png', height: 200),
+              Container(
+                width: 300,
+                child: TextFormField(
+                  validator: (value) {
+                    if(value!.isEmpty){
+                      return 'El nombre y apellidos debe estar relleno';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Nombre y apellidos',
+                    filled: true
+                  ),
+                ),
+              ),            
+              SizedBox(height: 10),
+              Container(
+                width: 300,
+                child: TextFormField(
+                  validator: (value) {
+                    if(value!.isEmpty){
+                      return 'El correo electrónico debe estar relleno';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Correo electrónico',
+                    filled: true
+                  ),
                 ),
               ),
-            ),            
-            SizedBox(height: 10),
-            Container(
-              width: 300,
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Correo electrónico',
-                  filled: true
+              SizedBox(height: 10),
+              Container(
+                width: 300,
+                child: TextFormField(
+                  validator: (value) {
+                    if(value!.isEmpty){
+                      return 'El número de telefono debe estar relleno';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Nº Telefono',
+                    filled: true
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              width: 300,
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Nº Telefono',
-                  filled: true
+              SizedBox(height: 10),
+              Container(
+                width: 300,
+                child: TextFormField(
+                  validator: (value) {
+                    if(value!.isEmpty){
+                      return 'La contraseña debe estar rellena';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Contraseña',
+                    filled: true
+                  ),
+                  obscureText: true,
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              width: 300,
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Contraseña',
-                  filled: true
-                ),
-                obscureText: true,
-              ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              width: 200,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: colorBoton
-                ),
-                onPressed: (){
-                  
-                }, 
-                child: Text(
-                  'Registrarse',
-                  style: TextStyle(color: colorLetraB)
-                )
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('¿Ya tienes cuenta?'),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                    context, 
-                    MaterialPageRoute(builder: (context) => PantallaLogin()),
-                    );      
+              SizedBox(height: 10),
+              Container(
+                width: 200,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: colorBoton
+                  ),
+                  onPressed: (){
+                    if(formKey.currentState!.validate()){
+                      //Debe ir la logica del resgitrarse
+                      
+                    }
                   }, 
-                  child: Text('Iniciar sesion')
-                )
-              ],
-            )
-          ],
+                  child: Text(
+                    'Registrarse',
+                    style: TextStyle(color: colorLetraB)
+                  )
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('¿Ya tienes cuenta?'),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                      context, 
+                      MaterialPageRoute(builder: (context) => PantallaLogin()),
+                      );      
+                    }, 
+                    child: Text('Iniciar sesion')
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
